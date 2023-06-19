8799c1591670fdfdb11e8eef2c8d1504a5fe43ac
dsound-zz <demiansims@gmail.com>
2023-06-19 11:59:08 -0400

    wip

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity"
 import { deskTool } from "sanity/desk"
+import schemas from "./sanity/schemas"
 
 const config = defineConfig({
   title: "music-portfolio",
@@ -8,6 +9,7 @@ const config = defineConfig({
   apiVersion: "2023-06-19",
   basePath: "/admin",
   plugins: [deskTool()],
+  schema: { types: schemas },
 })
 
 export default config
