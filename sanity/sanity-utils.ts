e6a2267bf76ce16612a5823b4e9a5ced16292325
dsound-zz <demiansims@gmail.com>
2023-06-21 11:00:31 -0400

    wip

--- sanity/sanity-utils.ts
+++ sanity/sanity-utils.ts
@@ -20,3 +20,24 @@ export async function getProjects(): Promise<Project[]> {
    }`
   )
 }
+
+export async function getProject(slug: string): Promise<Project> {
+  const client = createClient({
+    projectId: "y890hrro",
+    dataset: "production",
+    apiVersion: "2023-06-19",
+  })
+
+  return await client.fetch(
+    groq`*[_type == "project" && slug.current = $slug][0]{
+      _id,
+      _createdAt,
+      name,
+      "slug": slug.current,
+      "image": image.asset->url,
+      url,
+      content
+   }`,
+    { slug }
+  )
+}
