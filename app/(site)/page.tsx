353f6bbbbf625b7d3b28a7886cd1374d219c230e
dsound-zz <demiansims@gmail.com>
2023-06-24 11:23:38 -0400

    wip

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,39 +5,26 @@ import Link from "next/link"
 export default async function Home() {
   const projects = await getProjects()
   return (
-    <div className='max-w-5xl mx-auto py-20'>
-      <h1 className='text-7xl font-extrbold'>
-        Hello I&apos;m{" "}
-        <span className='bg-gradient-to-r from-sky-300 via-blue-600 to-red-700 bg-clip-text text-transparent'>
-          {" "}
-          Demian
-        </span>
-        !
-      </h1>
-      <p className='mt-3 text-xl text-gray-600'>
-        Hello everyone! Check out my music!
-      </p>
-      <h2 className='mt-24 font-bold text-gray-700 text-3xl'>My Projects</h2>
-      <div className='mt-5 grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+    <div className='max-w-5xl mx-auto py-10'>
+      <div className='mt-5 grid md:grid-cols-1 lg:grid-cols-1 gap-8'>
         {projects.map(({ _id, name, image, slug }) => (
-          <Link
-            key={_id}
-            href={`/projects/${slug}`}
-            className='border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition'
-          >
-            {image && (
-              <Image
-                src={image}
-                alt={name}
-                width={750}
-                height={300}
-                className='object-cover rounded-lg border border-gray-500'
-              />
-            )}
-            <div className='mt-2 font-extrabold bg-gradient-to-r  from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
-              {name}
-            </div>
-          </Link>
+          <div key={_id} className='flex-col'>
+            <h2 className='mt-24 font-bold text-gray-700 text-3xl'>{name}</h2>
+            <Link
+              href={`/projects/${slug}`}
+              className='p-4 hover:scale-105 hover:border-blue-500 transition'
+            >
+              {image && (
+                <Image
+                  src={image}
+                  alt={name}
+                  width={750}
+                  height={300}
+                  className='object-cover rounded-lg border border-gray-500'
+                />
+              )}
+            </Link>
+          </div>
         ))}
       </div>
     </div>
